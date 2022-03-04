1f152daeb5fbeea591a31104337dfa9396b9db6a
baggette4224 <58569691+Baggette@users.noreply.github.com>
2022-03-03 19:16:18 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client, Intents, Message } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+const prefix = "des"
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+client.on('ready', () => {
+    console.log('des is online')
+    client.user.setPresence({ activities: [{ name: 'Liberal quotes' }], status: 'idle' });
+});
+client.on('messageCreate', (message) => {
+    if(message.content.startsWith(prefix)){
+        let liberal =[
+            "yes",
+            "no",
+            "why would you ask this",
+            "blehhhh :nauseated_face: ",
+            "thats a dumb thing to ask",
+            "don't ask me",
+            "fuck off",
+            "try again later <:troll:900546362712666193>",
+            "uno reverse",
+            "skill issue",
+            "I am a extremely vocal leftist in high school"
+        ]
+        message.channel.send(`${liberal[Math.floor(Math.random() * liberal.length)]}`)
+    }
+});
+client.login(process.env.TOKEN);
